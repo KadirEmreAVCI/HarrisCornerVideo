5702ef4c0f574964de385b582aae0bfe44487636
0xZ3n0x <samienes06@gmail.com>
2026-04-13 19:18:28 +0300

    CMakePresets Linux and GPU Architecture RTX 5070

--- src/main.cu
+++ src/main.cu
@@ -11,12 +11,16 @@
 
 constexpr FilterSize filterSize = FilterSize::Size3x3;
 
-int main()
+int main(int argc, char** argv)
 {
-    const std::string inputVideoPath =
-        "C:/Users/User/Workspaces/VisualStudio/VS2026/HarrisCornerVideo/data/hacettepe_demo_input.mp4";
-    const std::string outputVideoPath =
-        "C:/Users/User/Workspaces/VisualStudio/VS2026/HarrisCornerVideo/data/hacettepe_demo_output.mp4";
+    if (argc != 1 && argc != 3)
+    {
+        std::cerr << "Usage: " << argv[0] << " [input_video output_video]" << std::endl;
+        return -1;
+    }
+
+    const std::string inputVideoPath  = (argc == 3) ? argv[1] : "../../data/hacettepe_demo_input.mp4";
+    const std::string outputVideoPath = (argc == 3) ? argv[2] : "../../data/hacettepe_demo_output.mp4";
 
     cv::VideoCapture cap(inputVideoPath);
     if (!cap.isOpened())
